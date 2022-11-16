abd11bec08b05b1cd254473c306de96894a53734
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-11-15 21:22:58 -0800

    added time meal and workout

--- app/create_workout.py
+++ app/create_workout.py
@@ -1,14 +1,14 @@
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.fields import DateField
+from wtforms.fields import DateField, TimeField
 from wtforms.validators import DataRequired, Length
 
-from datetime import date
+from datetime import date, time
 
 class CreateWorkout(FlaskForm):
     workout_name = StringField('Workout Name', validators = [DataRequired(),Length(min=3, max=32)])
     exercise = StringField('Exercise', validators = [DataRequired(), Length(min=3, max=32)])
     repititions = IntegerField('Number of Repititions', validators = [DataRequired()])
-    time_to_do = DateField('Time to do workout', format = '%Y-%m-%d', validators = [DataRequired()])
+    time_to_do = DateField('Day to do workout', format = '%Y-%m-%d', validators = [DataRequired()])
+    time = TimeField('Time to do workout', format = '%H:%M', validators = [DataRequired()])
     submit = SubmitField('Create Workout')
