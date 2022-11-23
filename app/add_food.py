98ce9b0a2721fb8065c5406e5197dc3de99b82c0
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-11-23 11:05:09 -0800

    added test meals, workout, users for test cases, changed edit meal

--- app/add_food.py
+++ app/add_food.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import FloatField, SubmitField, StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 from wtforms.fields import DateField, TimeField
 
 
 class AddFood(FlaskForm):
  name = StringField('Food Name', validators = [DataRequired(), Length(min=3, max=32)])
- calories = FloatField('Calorie Amount', validators = [DataRequired()])
- carbs = FloatField('Carbohydrate Amount (g)', validators = [DataRequired()])
- protien = FloatField('Protien Amount (g)', validators = [DataRequired()])
- fat = FloatField('Fat Amount (g)', validators = [DataRequired()])
+ calories = FloatField('Calorie Amount', validators = [DataRequired(), NumberRange(min = 0)])
+ carbs = FloatField('Carbohydrate Amount (g)', validators = [DataRequired(), NumberRange(min = 0)])
+ protien = FloatField('Protien Amount (g)', validators = [DataRequired(), NumberRange(min = 0)])
+ fat = FloatField('Fat Amount (g)', validators = [DataRequired(), NumberRange(min = 0)])
  submit = SubmitField("Add to meal")
 
