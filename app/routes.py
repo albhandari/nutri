b1dfb7fe96340094f7501d6ec00acd5f68cfc004
Alex Bhandari <alexbhandari@Alexs-MacBook-Pro.local>
2022-12-01 18:59:30 -0800

    Edit sign up page

--- app/routes.py
+++ app/routes.py
@@ -383,15 +383,23 @@ def edit_workout(workoutID):
 
 @appObj.route('/testing1', methods = ['GET', 'POST'])
 def testing1():
- login_form = LoginUser()
- if login_form.validate_on_submit():
-  user = User.query.filter_by(username = login_form.username.data).first()
-  if user != None:
-   if user.check_password(login_form.password.data) == True:
-    login_user(user)
-    return redirect(url_for('home'))
-   else:
-    flash('Incorrect password. Please try again.')
-  else:
-   flash('Username does not exist. Please enter an existing username')
- return render_template('testing.html', login_form = login_form)
+  accountForm = CreateUser()
+  if accountForm.validate_on_submit():
+    same_name = User.query.filter_by(username = accountForm.username.data).first()
+    if same_name == None:
+      user=User()
+      user.username=accountForm.username.data
+      user.email=accountForm.email.data
+      user.set_password(accountForm.password.data)
+      user.weight = accountForm.weight.data
+      user.fitness_goal = accountForm.fitness_goal.data
+      user.user_bio = accountForm.user_bio.data
+      #add to the database
+      db.session.add(user)
+      db.session.commit()
+      #take the user back to login screen so they can log in with their new account
+      flash('Your account has been created successfully')
+      return redirect('/')
+    else:
+     flash('That username has been taken. Please try again') #no duplicate usernames
+  return render_template('testing.html', accountForm = accountForm)
