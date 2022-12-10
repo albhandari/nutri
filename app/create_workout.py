2f41eb8e5edcd2687b94a4752b179e58bec34641
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2022-12-09 23:02:01 -0800

    Update create_workout.py

--- app/create_workout.py
+++ app/create_workout.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length
 from datetime import date, time
 
 class CreateWorkout(FlaskForm):
-    workout_name = StringField('Workout Name', validators = [DataRequired(),Length(min=3, max=32)])
+    workout_name = StringField('Workout Name', validators = [DataRequired(),Length(min=3, max=32)], render_kw={"placeholder": "Ex: yofit, between 3 and 32 characters"})
     exercise = StringField('Exercise', validators = [DataRequired(), Length(min=3, max=32)])
     repititions = IntegerField('Number of Repititions', validators = [DataRequired()])
     time_to_do = DateField('Day to do workout', format = '%Y-%m-%d', validators = [DataRequired()])
